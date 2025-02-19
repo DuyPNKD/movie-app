f8f608eadc98e11c4d3de25c79dc0543e4fb318e
DuyPNKD <165270115+DuyPNKD@users.noreply.github.com>
2025-02-19 23:45:52 +0700

    Update detail screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
-import MovieDetailScreen from "./src/screens/MovieDetailScreen";
+import MovieDetailScreen from "./src/screens/detail/MovieDetailScreen";
 
 const Stack = createStackNavigator();
 
